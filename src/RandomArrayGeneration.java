e91b05de2beb7e4495d5bd9e5e86449499dffd18
jderby <jpderby1228@gmail.com>
2018-09-25 13:39:03 -0400

    Initial commit

--- src/RandomArrayGeneration.java
+++ src/RandomArrayGeneration.java
@@ -1,11 +1,24 @@
-import java.util.Random;
+import java.util.Arrays;
 
 public class RandomArrayGeneration {
     public static void main(String[] args) {
-        Random random = new Random();
+        int max = 10;
+        int min = 1;
+        int maxLength = 10;
+        int minLength = 1;
+        int lengthRange = (maxLength - minLength) + minLength;  //Sets range for array length and the numbers within, following (max - min) + min format
+        int numRange = (max - min) + min;
+
+        int arrayLength = (int)(Math.random() * lengthRange) + minLength;  //Generates
+        int[] randomStorageTM = new int[arrayLength];
+
+        for(int i = 0; i < randomStorageTM.length; i++) {
+            randomStorageTM[i] = (int)(Math.random() * numRange) + min;
+        }
+        System.out.print(Arrays.toString(randomStorageTM));
 
     }
 }
 
 
-}
+
