5d20dfb9b6c64763001d4992b00286512aa6d4c8
jderby <jpderby1228@gmail.com>
2018-11-19 13:25:48 -0500

    Initial commit

---
+++ src/JacksonDerbyPersonalityTest.java
@@ -0,0 +1,30 @@
+public class JacksonDerbyPersonalityTest {
+    public static void main(String args[]) {
+        //Step 1: Prompt for input file until given a valid file
+
+        //Step 2: Request an output file to which to print the results
+
+        //Step 3: Scan input for name and print name to output
+
+        //Step 4: Go through input and store the number of A answers for each of the four categories and the number of B answers for each of the four categories
+
+        //Step 5: Print these values to output formatted to match example
+
+        //Step 6: Compare percentage of B Answers to total answers and print said percentage to output
+
+        //Step 7: Based on percentage, print to output the letter corresponding to their personality type
+
+        //Step 8: Repeat steps 3-7 until there are no more names to process
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
--- src/JacksonDerbySum.java
+++ src/JacksonDerbySum.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class JacksonDerbySum {
@@ -12,46 +11,31 @@ public class JacksonDerbySum {
         //Step 1: Find largest value in line and create an array to match its length
 
         while(input.hasNextLine()) {
-
             String line = input.nextLine();       //Creates a scanner for the line of the file
             Scanner lineSC = new Scanner(line);
-            boolean needsPlusSign = false;
+            boolean needsPlusSign = false;   //Boolean that keeps track of whether a plus sign is needed between the printed values
 
-            int[] finalNumber = new int[findMaxDigitLength(input, line)];
+            int[] finalNumber = new int[findMaxDigitLength(input, line)]; //Creates an array to keep track of sum based on the largest number of digits on the line
 
             //Step 2: While there is a value to be processed, add it onto the array digit by digit
             while (lineSC.hasNext()) {
 
-                if (needsPlusSign) {
+                if (needsPlusSign) {   //Checks boolean(Always false first time, true for all subsequent times)
                     System.out.print(" + ");
                 }
 
-                finalNumber = addNextValueInFile(finalNumber, lineSC);
+                finalNumber = addNextValueInFile(finalNumber, lineSC); //Adds value onto array
 
-                needsPlusSign = true;
-
+                needsPlusSign = true;   //Toggles boolean to add variables before subsequent values
             }
 
+            //Step 3: Once completed with the additions, go through array and get rid of values ten or more, incrementing the next value place
 
-            //Step 3: Once completed with the additions, go from right to left checking if the value is more than ten until reaching the last value of the array
+            finalNumber = incrementArray(finalNumber);
 
-            for (int i = finalNumber.length - 1; i > 0; i--) {
-                while (finalNumber[i] >= 10) {
-                    finalNumber[i - 1]++;
-                    finalNumber[i] = finalNumber[i] - 10;  //Step 3a: If it is, subtract ten from the value, and increment the value to the left by one
-                }
+            //Step 4: Prints output
 
-            }
-
-            //Step 4: Print out each integer individually on the same line to form the output
-
-            System.out.print(" = ");
-
-            for (int i = 0; i < finalNumber.length; i++) {
-                System.out.print(finalNumber[i]);
-            }
-
-            System.out.println();
+            printArray(finalNumber);
 
         }
 
@@ -75,21 +59,40 @@ public class JacksonDerbySum {
     }
 
     public static int[] addNextValueInFile(int[] finalNumber, Scanner lineSC) {
-        String numberToAdd = lineSC.next();
+        String numberToAdd = lineSC.next(); //Creates a string to be processed, converted to int, and added onto array
 
-        System.out.print(numberToAdd);
+        System.out.print(numberToAdd);  //Prints this number to create a log of added numbers in the output
 
-        int initialLength = numberToAdd.length();
+        int initialLength = numberToAdd.length(); //Length of string
 
-        for(int i = 1; i <= initialLength; i++) {
+        for(int i = 1; i <= initialLength; i++) { //Going from right to left in the array, take the character at the right end of the string, convert it to an int, and add it to its proper value place
             finalNumber[finalNumber.length - i] = finalNumber[finalNumber.length - i] + Character.getNumericValue(numberToAdd.charAt(numberToAdd.length() - 1));
 
-            numberToAdd = numberToAdd.substring(0, numberToAdd.length() - 1);
-        }
-
-
+            numberToAdd = numberToAdd.substring(0, numberToAdd.length() - 1);  //Remove the character that was just processed from the string
+        }  //For loop goes for number of digits to process in string/the length of the string, ensuring no out of bounds errors
 
         return finalNumber;
+    }
+
+    public static int[] incrementArray(int[] finalNumber) {  //Goes from right to left checking if the value is more than or equal to ten until reaching the last value of the array
+        for (int i = finalNumber.length - 1; i > 0; i--) {
+            while (finalNumber[i] >= 10) {
+                finalNumber[i - 1]++;
+                finalNumber[i] = finalNumber[i] - 10;  //If it is >=10, subtract ten from the value, and increment the value to the left by one
+            }
+
+        }
+        return finalNumber;
+    }
+
+    public static void printArray(int[] finalNumber) {  //Prints out the equals sign and each integer individually on the same line to form the output
+        System.out.print(" = ");
+
+        for (int i = 0; i < finalNumber.length; i++) {
+            System.out.print(finalNumber[i]);
+        }
+
+        System.out.println();
     }
 
 
