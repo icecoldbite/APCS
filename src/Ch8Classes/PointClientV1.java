2cfc4cf3b9ba70d5361ae7600cfe70608da1990b
jderby <jpderby1228@gmail.com>
2018-11-27 12:57:31 -0500

    Initial commit

---
+++ src/Ch8Classes/PointClientV1.java
@@ -0,0 +1,13 @@
+package Ch8Classes;
+
+public class PointClientV1 {
+    public static void main(String[] args) {
+        PointV1 p1 = new PointV1(); //Creates a point at (0,0)
+
+
+    }
+
+
+
+
+}
---
+++ src/Ch8Classes/PointV1.java
@@ -0,0 +1,12 @@
+package Ch8Classes;
+
+public class PointV1 {
+    public int x; //States fields for point class
+    public int y;
+
+    public void translate(int x, int y) {  //Behaviors for the Point class
+
+    }
+
+
+}
