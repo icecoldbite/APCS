929e23b355e3e2dc4ddc9c7d140e7c2d07624553
jderby <jpderby1228@gmail.com>
2019-01-30 13:37:33 -0500

    Commited

--- src/CH10_ArrayList/Vocabulary1.java
+++ src/CH10_ArrayList/Vocabulary1.java
@@ -1,4 +1,11 @@
 package CH10_ArrayList;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Vocabulary1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner file1 = new Scanner(new File("C:\\Users\\jderby\\APCS\\src\\CH10_ArrayList\\text1.txt"));
+    }
 }
