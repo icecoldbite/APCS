bab64ef88b301876acdb5dfadfc13c3879565dc0
jderby <jpderby1228@gmail.com>
2019-04-24 13:32:58 -0400

    Commited

--- src/SortingAlgorithms/InsertionSortPractice.java
+++ src/SortingAlgorithms/InsertionSortPractice.java
@@ -1,7 +1,20 @@
 package SortingAlgorithms;
 
+import java.util.Arrays;
+
 public class InsertionSortPractice {
     public static void main(String[] args) {
         int [] numArray = { 101, 59, 65, 32, 45, 12, -3, -24, 87};
+    }
+
+    public static void insertionSort(int[] array) {
+        for(int i = 1; i < array.length; i++) {
+            int j = i;
+            while(j > 0 && array[j-1] > array[j]) {
+                int temp;
+            }
+        }
+
+
     }
 }
