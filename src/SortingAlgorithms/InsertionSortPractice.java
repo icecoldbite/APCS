66021c6104bd655835a6cede46b5298e58b8e2b3
icecoldbite <jpderby1228@gmail.com>
2019-04-24 15:59:47 -0400

    Version 2.0

--- src/SortingAlgorithms/InsertionSortPractice.java
+++ src/SortingAlgorithms/InsertionSortPractice.java
@@ -5,16 +5,23 @@ import java.util.Arrays;
 public class InsertionSortPractice {
     public static void main(String[] args) {
         int [] numArray = { 101, 59, 65, 32, 45, 12, -3, -24, 87};
+        insertionSort(numArray);
+        System.out.println(Arrays.toString(numArray));
     }
 
     public static void insertionSort(int[] array) {
         for(int i = 1; i < array.length; i++) {
-            int j = i;
-            while(j > 0 && array[j-1] > array[j]) {
-                int temp;
-            }
-        }
+            int temp = array[i];
+            int j = i - 1;
 
+            while(j >= 0 && array[j] > temp) {
+                array[j + 1] = array[j];
+                j = j - 1;
+
+            }
+
+            array[j + 1] = temp;
+        }
 
     }
 }
